b9c1e0c79ea1d3a782470c70cb3846927045d13e
Prem Kamble <premkamble099@gmail.com>
2025-10-07 23:52:31 +0530

    Update generative model to 'gemini-2.0-flash'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const apikey = import.meta.env.VITE_GEMINI_API_KEY;
 
 
 const genAI = new GoogleGenerativeAI(apikey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 const App = () => {
 
